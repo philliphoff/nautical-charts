ebb872cb373402cc2ade246040a9df0a01f4a4ec
Phillip Hoff <phillip@orst.edu>
2021-06-22 21:36:09 -0700

    Update dependencies.

--- src/memoryStream.ts
+++ src/memoryStream.ts
@@ -2,14 +2,11 @@
 // Licensed under the MIT license.
 
 import { Readable, ReadableOptions } from 'stream';
-import { debug } from 'debug';
 
 /**
  * A readable stream for an in-memory byte array.
  */
 export class MemoryStream extends Readable {
-    private static readonly log = debug('nautical-charts:memory-stream');
-
     private position = 0;
 
     /**
@@ -24,8 +21,6 @@ export class MemoryStream extends Readable {
     _read(size: number) {
         if (this.position < this.contents.length) {
             size = Math.min(size, this.contents.length - this.position);
-
-            MemoryStream.log(`Reading [${this.position}..${this.position + size}) (${size} bytes).`);
 
             // Create a Buffer that exposes a range of (i.e. does not copy) the original array...
             const buffer = Buffer.from(this.contents.buffer, this.position, size);
